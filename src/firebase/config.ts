97448bd69ecab44309dc7681e11a31fd0493e724
Sunny Suhas <sunnysuhas108@gmail.com>
2026-01-04 11:07:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8174873780-5777a",
+  "appId": "1:581630167564:web:b0fe39f1d214a417363c29",
+  "apiKey": "AIzaSyCZQesNtnWRh97v7kSqvTQOKwklojqkFJI",
+  "authDomain": "studio-8174873780-5777a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "581630167564"
+};
